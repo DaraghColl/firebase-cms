434edfc8d0a7c6a4edcbd637ceedb0c91a770a7d
DaraghColl <daraghcoll@hotmail.com>
2021-02-24 11:43:41 +0000

    navbar & routing, & swap primeng for ng material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ContentBuilderComponent } from './pages/content-builder/content-builder.component';
+import { ContentEditorComponent } from './pages/content-editor/content-editor.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'builder', component: ContentBuilderComponent },
+  { path: 'editor', component: ContentEditorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ng material
+import { MatIconModule } from '@angular/material/icon';
+import { ContentBuilderComponent } from './pages/content-builder/content-builder.component';
+import { ContentEditorComponent } from './pages/content-editor/content-editor.component';
+import { LoginComponent } from './pages/login/login.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContentBuilderComponent,
+    ContentEditorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
